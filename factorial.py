9865032bfdbf2ca786a5c37fdabaf781ace9e472
rampigorana <scottjaff@gmail.com>
2020-01-10 13:06:39 +0530

    Added new file

---
+++ factorial.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 12:14:54 2020
+
+@author: user
+"""
+import math 
+s=int(input("enter any number for factorial:-"))
+print( "The factorial is :- ", math.factorial(s))
